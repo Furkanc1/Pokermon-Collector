8d3e950792a4e68118931d4c00c7a4d81e23cb35
Furkan Coskun <furkdurk7@aol.com>
2024-03-15 13:08:22 -0400

    some styling / components out, Need to create navbar + Footer bar

---
+++ pokermon/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../components/style.css';
+const Main = () => {
+    return(
+        <>
+            <div className="container">
+                <div className="left-content">
+                    Pokemon Cards
+                </div>
+                <div className="right-content">
+                    pokemonInfo
+                </div>
+            </div>
+        
+        </>
+    )
+}
+
+export default Main;
